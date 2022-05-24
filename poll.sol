c9640e99c3e6002deb41760364d5c98657b5d0b1
hamed <hmd.nasr@gmail.com>
2022-05-24 08:55:04 -0400

    check name repeat

--- poll.sol
+++ poll.sol
@@ -15,11 +15,29 @@ contract Poll{
 
     function addCandidate(bytes memory candidateName) public{
         require(msg.sender == owner);
+        require(! isNameRepeat(candidateName));
         Candidate memory _candidate;
         _candidate.name = candidateName;
         _candidate.votes = 0;
         candidates.push(_candidate);
     }
 
+    function equalstrings (bytes memory a, bytes memory b) 
+    private pure returns(bool){
+        if (a.length != b.length)
+            return false;
+        else
+            for (uint i=0; i < a.length; i++){
+                if (a[i] != b[i])
+                    return false;
+            }
+            return true;
+    }
 
+    function isNameRepeat(bytes memory name) private view returns(bool){
+        for (uint i; i < candidates.length; i++)
+            if (equalstrings(candidates[i].name, name))
+                return true;
+        return false;
+    }
 }
