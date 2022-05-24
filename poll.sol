1bc30b8a43dad27d989f706fd48eb309e7ffef25
hamed <hmd.nasr@gmail.com>
2022-05-24 07:47:48 -0400

    add candidate

--- poll.sol
+++ poll.sol
@@ -1,10 +1,25 @@
 pragma solidity ^0.5.1;
 contract Poll{
+    address owner;
     struct Candidate{
-        string name;
+        bytes name;
         uint votes;
     }
     Candidate[] candidates;
     mapping(address => bool) voters;
+
+    constructor() public{
+        owner = msg.sender;
+        
+    } 
+
+    function addCandidate(bytes memory candidateName) public{
+        require(msg.sender == owner);
+        Candidate memory _candidate;
+        _candidate.name = candidateName;
+        _candidate.votes = 0;
+        candidates.push(_candidate);
+    }
+
 
 }
