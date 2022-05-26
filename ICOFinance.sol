a3d631f1becd80ed23827e960f8d06e7aef6965e
hamed <hmd.nasr@gmail.com>
2022-05-26 06:11:22 -0400

    start

---
+++ ICOFinance.sol
@@ -0,0 +1,12 @@
+pragma solidity ^0.5.1;
+
+contract ICOFinance{
+    uint shareValue = 1e15;
+    uint shareNum = 1e9;
+    struct ShareHolder {
+        address adr;
+        uint sharePur;
+    }    
+
+
+} 
