d0a6191462edfd04ff2eea20ce4e45053b1fb2b7
hamed <hmd.nasr@gmail.com>
2022-05-28 00:12:44 -0400

    pay function

--- ICOFinance.sol
+++ ICOFinance.sol
@@ -2,11 +2,27 @@ pragma solidity ^0.5.1;
 
 contract ICOFinance{
     uint shareValue = 1e15;
-    uint shareNum = 1e9;
+    uint sharesNeeded = 1e9;
+    uint Deadline = now + 30 days;
+    bool EnoughSharesAquired = false;
     struct ShareHolder {
-        address adr;
-        uint sharePur;
-    }    
+        uint shares;
+    } 
+    mapping (address => ShareHolder) ShareHolders;
 
+    function() external payable{
+        require(!EnoughSharesAquired,'maximum number of shares purchased!');
+        require(now <= Deadline);
+        require((msg.value % shareValue == 0) && (msg.value > 0), 
+                'please send multiply of 0.001 Ether');
+        require(sharesNeeded - address(this).balance / shareValue >= msg.value,
+                'not enough share to purchase!');
 
+        ShareHolder memory sh;
+        sh.shares = msg.value/shareValue + ShareHolders[msg.sender].shares ;
+        ShareHolders[msg.sender] = sh;
+        // ShareHolder memory sh = ShareHolder({shares: msg.value/shareValue});
+        if ((address(this).balance +msg.value) /shareValue == sharesNeeded)
+            EnoughSharesAquired = true;
+    }
 } 
