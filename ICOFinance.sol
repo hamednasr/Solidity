a4c1c82690eac58d68800ffb462f338d99e10b37
hamed <hmd.nasr@gmail.com>
2022-05-28 03:10:48 -0400

    transfer to owner

--- ICOFinance.sol
+++ ICOFinance.sol
@@ -1,14 +1,43 @@
 pragma solidity ^0.5.1;
 
 contract ICOFinance{
+    address owner;
+    constructor() public{
+        owner = msg.sender;
+    }
+    
     uint shareValue = 1e15;
-    uint sharesNeeded = 1e9;
+    // uint public sharesNeeded = 1e9;
+    uint public sharesNeeded = 1e4;
+    uint sharesAquired;
     uint Deadline = now + 30 days;
     bool EnoughSharesAquired = false;
+    bool TransferedtoOwner = false;
     struct ShareHolder {
         uint shares;
     } 
     mapping (address => ShareHolder) ShareHolders;
+
+    function payRefund() external {
+        require(ShareHolders[msg.sender].shares > 0,'you dont have any shares!');
+        require(now > Deadline,'deadline not reached');
+        require(! EnoughSharesAquired,'refund not possible');
+        ShareHolders[msg.sender].shares = 0; 
+        msg.sender.transfer(ShareHolders[msg.sender].shares * shareValue);
+    }  
+
+    function TransfertoOwner() external {
+        require(EnoughSharesAquired,'not enough shares!');
+        require(now > Deadline,'deadline not reached!');
+        require(owner ==msg.sender);
+        require(!TransferedtoOwner,'money transfered to owner before!');
+        TransferedtoOwner = true;
+        msg.sender.transfer(address(this).balance);        
+    }
+
+    function AquieredFund() external view returns(uint){
+        return address(this).balance/shareValue;
+    }
 
     function() external payable{
         require(!EnoughSharesAquired,'maximum number of shares purchased!');
@@ -26,11 +55,4 @@ contract ICOFinance{
             EnoughSharesAquired = true;
     }
 
-    function giveRefund() external {
-        require(ShareHolders[msg.sender].shares > 0,'you dont have any shares!');
-        require(now > Deadline,'deadline not reached');
-        require(! EnoughSharesAquired,'refund not possible');
-        ShareHolders[msg.sender].shares = 0; 
-        msg.sender.transfer(ShareHolders[msg.sender].shares * shareValue);
-    }  
 } 
