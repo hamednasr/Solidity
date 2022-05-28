fa894f0acb56d4510de3eceeee5fcda585d97d66
hamed <hmd.nasr@gmail.com>
2022-05-28 07:28:56 -0400

    refund from owner

--- ICOFinance.sol
+++ ICOFinance.sol
@@ -8,23 +8,35 @@ contract ICOFinance{
     
     uint shareValue = 1e15;
     // uint public sharesNeeded = 1e9;
-    uint public sharesNeeded = 1e4;
+    uint public sharesNeeded = 100;
     uint sharesAquired;
-    uint Deadline = now + 30 days;
+    uint Deadline = now + 240 seconds;
     bool EnoughSharesAquired = false;
     bool TransferedtoOwner = false;
     struct ShareHolder {
         uint shares;
+        uint refundtime;
     } 
     mapping (address => ShareHolder) ShareHolders;
+    address payable[] ShareHolderlist; 
 
-    function payRefund() external {
+    function PayMyRefund() external {
         require(ShareHolders[msg.sender].shares > 0,'you dont have any shares!');
         require(now > Deadline,'deadline not reached');
         require(! EnoughSharesAquired,'refund not possible');
         ShareHolders[msg.sender].shares = 0; 
+        ShareHolders[msg.sender].refundtime = now; 
         msg.sender.transfer(ShareHolders[msg.sender].shares * shareValue);
     }  
+
+    function PayHolderRefund() external {
+        require(owner ==msg.sender);
+        for (uint i = 0; i < ShareHolderlist.length; i++){
+            ShareHolders[ShareHolderlist[i]].shares = 0;
+            ShareHolderlist[i].transfer(ShareHolders[ShareHolderlist[i]].shares * shareValue);
+            ShareHolders[ShareHolderlist[i]].refundtime = now;
+            }
+    }
 
     function TransfertoOwner() external {
         require(EnoughSharesAquired,'not enough shares!');
@@ -52,10 +64,14 @@ contract ICOFinance{
                 'please send multiply of 0.001 Ether');
         require(sharesNeeded - address(this).balance / shareValue >= msg.value,
                 'not enough share to purchase!');
-
+        
         ShareHolder memory sh;
         sh.shares = msg.value/shareValue + ShareHolders[msg.sender].shares ;
         ShareHolders[msg.sender] = sh;
+
+        if (ShareHolders[msg.sender].shares == 0)
+            ShareHolderlist.push(msg.sender);
+
         // ShareHolder memory sh = ShareHolder({shares: msg.value/shareValue});
         if ((address(this).balance +msg.value) /shareValue == sharesNeeded)
             EnoughSharesAquired = true;
