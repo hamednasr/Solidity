511603d50b107b2524979796d767bb716ecceaf7
Hamed Nasr <76178686+hamednasr@users.noreply.github.com>
2022-06-09 03:27:41 -0400

    connect to ganache

--- web3_py_simple_storage/deploy.py
+++ web3_py_simple_storage/deploy.py
@@ -1,6 +1,7 @@
 from solcx import compile_standard, install_solc
 import os
 import json
+from web3 import Web3
 # from dotenv import load_dotenv
 
 # load_dotenv()
@@ -10,8 +11,8 @@ with open("./SimpleStorage.sol", "r") as file:
     simple_storage_file = file.read()
 
 # We add these two lines that we forgot from the video!
-# print("Installing...")
-# install_solc("0.6.0")
+print("Installing...")
+install_solc("0.6.0")
 
 # Solidity source code
 compiled_sol = compile_standard(
@@ -33,14 +34,17 @@ with open("compiled_code.json", "w") as file:
     json.dump(compiled_sol, file)
 
 
-# get bytecode
 bytecode = compiled_sol["contracts"]["SimpleStorage.sol"]["SimpleStorage"]["evm"][
     "bytecode"
 ]["object"]
 
-# get abi
 abi = json.loads(
     compiled_sol["contracts"]["SimpleStorage.sol"]["SimpleStorage"]["metadata"]
 )["output"]["abi"]
 
-print(abi)
+# connect to ganache
+w3 = Web3(Web3.HTTPProvider('HTTP://127.0.0.1:7545'))
+chainID = 5777
+MyAddress = '0x0AF09a6e56FAAC1bBd2Aeb7DCe7a7DdAE77Ee6f6'
+PrivateKey = '0x78e3c71899a9c178a5f08857f95439ee2dbedc878969f2dcd085d0f95cafbe57'
+
