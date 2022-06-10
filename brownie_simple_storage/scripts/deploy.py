12e6fe280235937172db886d553032c467bf7eec
Hamed Nasr <76178686+hamednasr@users.noreply.github.com>
2022-06-10 10:24:27 -0400

    import and deploy contract

--- brownie_simple_storage/scripts/deploy.py
+++ brownie_simple_storage/scripts/deploy.py
@@ -1,12 +1,16 @@
-from brownie import accounts, config
+from brownie import accounts, config, SimpleStorage
 import os
 def deploy_simple_storage():
-    # account = accounts[0]
+    account = accounts[0]
     # account = accounts.load('david')
     # account = accounts.add(os.getenv('PRIVATE_KEY'))
     # print(account)
-    account = accounts.add(config["wallets"]["from_key"])
-    print(account)
+    # account = accounts.add(config["wallets"]["from_key"])
+    # print(account)
+     
+    simple_storage = SimpleStorage.deploy({'from':account})
+
+    print(simple_storage)
 
 def main():
     deploy_simple_storage()
