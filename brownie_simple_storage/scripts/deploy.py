78ea23f9f9670c102fdaa553937056ddcf869c77
Hamed Nasr <76178686+hamednasr@users.noreply.github.com>
2022-06-10 10:15:31 -0400

    brownie config

--- brownie_simple_storage/scripts/deploy.py
+++ brownie_simple_storage/scripts/deploy.py
@@ -1,10 +1,11 @@
-from brownie import accounts
+from brownie import accounts, config
 import os
-
 def deploy_simple_storage():
     # account = accounts[0]
     # account = accounts.load('david')
-    account = accounts.add(os.getenv('PRIVATE_KEY'))
+    # account = accounts.add(os.getenv('PRIVATE_KEY'))
+    # print(account)
+    account = accounts.add(config["wallets"]["from_key"])
     print(account)
 
 def main():
