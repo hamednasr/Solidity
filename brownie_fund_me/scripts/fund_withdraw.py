6e77d4d238a98a2c86105e7fb066fac98724189e
Hamed Nasr <76178686+hamednasr@users.noreply.github.com>
2022-06-13 08:43:50 -0400

    test fund

--- brownie_fund_me/scripts/fund_withdraw.py
+++ brownie_fund_me/scripts/fund_withdraw.py
@@ -2,16 +2,19 @@ from traceback import print_tb
 from brownie import FundMe
 from scripts.helpful_scripts import get_account
 
+account = get_account()
 
 def fund():
     fund_me = FundMe[-1]
-    print(fund_me.address)
-    account = get_account()
     entrance_fee = fund_me.getEntranceFee()
     print(f'the current entrace fee is: {entrance_fee}')
     print('Funding...')
     fund_me.fund({'from':account, 'value':entrance_fee})
 
+def withdraw():
+    fund_me = FundMe[-1]
+    fund_me.withdraw({'from':account})
 
 def main():
     fund()
+    withdraw()
